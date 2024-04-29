4abde4e36a209a14b1fb9001f4f27fae831859a6
Cristopher <cristopheralexis.chavez@alumnos.udg.mx>
2024-04-29 16:47:53 -0600

    Añadiendo activity de directorio y navegación entre activities

--- app/src/main/java/com/example/qcimobile/DirectoryActivity.java
+++ app/src/main/java/com/example/qcimobile/DirectoryActivity.java
@@ -3,7 +3,6 @@ package com.example.qcimobile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +10,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Menu extends AppCompatActivity {
-    ImageButton btnMenuIcon;
-    ImageButton btnUserIcon;
-    ImageView imgDirectory;
+public class DirectoryActivity extends AppCompatActivity {
 
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_directory);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,29 +29,17 @@ public class Menu extends AppCompatActivity {
     }
 
     public void loadViews(){
-        btnMenuIcon = (ImageButton) findViewById(R.id.btnMenuIcon);
-        btnUserIcon = (ImageButton) findViewById(R.id.btnUserIcon);
-        imgDirectory = (ImageView) findViewById(R.id.imgDirectory);
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
     }
 
     public void setEventListeners(){
-
-        btnUserIcon.setOnClickListener(event -> {
-            changeLoginActivity();
-        });
-
-        imgDirectory.setOnClickListener(event -> {
-            changeDirectoryActivity();
+        btnBack.setOnClickListener(event ->{
+            changeMenuActivity();
         });
     }
 
-    public void changeLoginActivity(){
-        Intent nextActivity = new Intent(this, MainActivity.class);
-        startActivity(nextActivity);
-    }
-
-    public void changeDirectoryActivity(){
-        Intent nextActivity = new Intent(this, DirectoryActivity.class);
+    public void changeMenuActivity(){
+        Intent nextActivity = new Intent(this, Menu.class);
         startActivity(nextActivity);
     }
 }
